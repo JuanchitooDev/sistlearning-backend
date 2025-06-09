2026c87d8cc0a9f0f2cec30fb5e8e3840ed45e30
JuanRacchumi <juan.racchumi.dev@gmail.com>
2025-06-09 14:44:53 -0500

    Update long field token

--- migrations/20250609174650-update-usuario-token.js
+++ migrations/20250609174650-update-usuario-token.js
@@ -10,7 +10,7 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
     await queryInterface.changeColumn('Usuario', 'token', {
-      type: Sequelize.STRING(150),
+      type: Sequelize.STRING(255),
       allowNull: true
     })
   },
